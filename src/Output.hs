dc5cfeea6fd64295730841c892cae2ae4cad1a6b
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-07-25 23:48:09 +0200

    Variation output.

---
+++ src/Output.hs
@@ -0,0 +1,24 @@
+
+module Output where
+
+import Common.Interpreter hiding (Term, interp)
+
+type O a = (String, a)
+returnO a = ("", a)
+m `bindO` k = let (r,a) = m; (s,b) = k a in (r ++ s, b)
+showO (s, a) = "Output: " ++ s ++ " Value: " ++ showval a
+
+outO :: Value -> O ()
+outO a = (showval a ++ "; ", ())
+
+data Term = Var Name
+          | Con Int
+          | Add Term Term
+          | Lam Name Term
+          | App Term Term
+          | Out Term
+
+interp (Out u) e = interp u e `bindO` (\a ->
+                    outO a `bindO` (\() ->
+                    returnO a))
+
